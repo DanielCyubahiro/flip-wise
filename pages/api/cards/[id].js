1a98ef8b1d83f243aae3599c29cfd8a0bb4c1738
Daniel <cyubahirodaniel@gmail.com>
2025-05-02 09:56:15 +0200

    Add delete functionality in the backend

---
+++ pages/api/cards/[id].js
@@ -0,0 +1,18 @@
+import dbConnect from '@/config/database';
+import Cards from '@/config/models/cards';
+
+export default async function handler(request, response) {
+  try {
+    await dbConnect();
+    const {id} = request.query;
+    if (request.method === 'DELETE') {
+      await Cards.findByIdAndDelete(id);
+      return response.status(200).
+          json({status: 'Card deleted successfully'});
+    }
+    return response.status(405).json({message: 'Method not allowed'});
+  } catch (error) {
+    console.error('API Error:', error);
+    return response.status(500).json({error: error.message});
+  }
+}
