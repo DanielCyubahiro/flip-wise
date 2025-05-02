b4c58de6a70d4d773d14aa88b3cd9a10c9d8f86d
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-05-02 15:57:47 +0200

    update main wrapper of the page

--- component/StyledWrapper.jsx
+++ component/StyledWrapper.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const StyledWrapper = styled.section`
+export const StyledWrapper = styled.main`
   display: flex;
   justify-content: center;
   align-items: center;
