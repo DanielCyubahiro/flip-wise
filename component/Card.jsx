d0aa46965074598b59a58c8ee851c32cdf44b248
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-05-02 15:18:06 +0200

    Solve the comments from the coach

--- component/Card.jsx
+++ component/Card.jsx
@@ -1,13 +1,6 @@
 import styled from "styled-components";
-import { StyledH2 } from "./StyledHeadings";
 
-const CardContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  margin: 20px 0;
-`;
-
-const CardBox = styled.div`
+const CardBox = styled.section`
   background-color: #ffffff;
   border-radius: 16px;
   box-shadow: 0 4px 20px rgba(0, 0, 0, 0.08);
@@ -28,13 +21,11 @@ const QuestionText = styled.p`
   color: #333;
 `;
 
-export default function Card({ question, collectionTitle }) {
+export default function Card({ question, collectionName }) {
   return (
-    <CardContainer>
-      <CardBox>
-        <StyledH2>{collectionTitle}</StyledH2>
-        <QuestionText>{question}</QuestionText>
-      </CardBox>
-    </CardContainer>
+    <CardBox>
+      <span>#{collectionName}</span>
+      <QuestionText>{question}</QuestionText>
+    </CardBox>
   );
 }
---
+++ component/StyledWrapper.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledWrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 1rem;
+`;
