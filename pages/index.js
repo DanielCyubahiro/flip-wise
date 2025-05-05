8aa880be6f5f52cd5ebac7b076d1aea04621feb3
Daniel <cyubahirodaniel@gmail.com>
2025-05-05 10:17:32 +0200

    Add successful message

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,49 @@
-import useSWR from "swr";
-import Card from "@/component/Card";
-import {StyledH1} from "@/component/StyledHeadings";
-import {StyledWrapper} from "@/component/StyledWrapper";
+import useSWR from 'swr';
+import Card from '@/component/Card';
+import {StyledH1} from '@/component/StyledHeadings';
+import {StyledWrapper} from '@/component/StyledWrapper';
+import StyledAlert from '@/component/StyledAlert';
+import {useState} from 'react';
 
-export default function HomePage({ fetcher }) {
-  const {data: cards, isLoading, error, mutate} = useSWR("/api/cards", fetcher);
+export default function HomePage({fetcher}) {
+  const [alert, setAlert] = useState({
+    show: false,
+    message: "",
+    type: 'info'
+  });
+  const {data: cards, isLoading, error, mutate} = useSWR('/api/cards', fetcher);
 
   if (isLoading) return <div>Loading cards...</div>;
   if (error) return <div>Failed to load cards. Error: {error.message}</div>;
   const handleDelete = async (id) => {
     const response = await fetch(`/api/cards/${id}`, {
-      method: "DELETE",
+      method: 'DELETE',
     });
-
     if (response.ok) {
+      setAlert({
+        show: true,
+        message: 'Card deleted successfully!',
+        type: 'success'
+      })
       mutate();
+    } else {
+      setAlert({
+        show: true,
+        message: 'Could not delete card!',
+        type: 'error'
+      })
     }
   };
   return (
       <StyledWrapper>
+        {alert.show && (
+            <StyledAlert
+                message={alert.message}
+                type="success"
+                onClose={() => setAlert({...alert, show: false})}
+                duration={3000}
+            />
+        )}
         <StyledH1>All Cards List</StyledH1>
         {cards.map((card) => {
           return (
@@ -27,7 +52,7 @@ export default function HomePage({ fetcher }) {
                   question={card.question}
                   collectionName={card.collectionId?.title}
                   onDelete={() => handleDelete(card._id)}
-              ></Card>
+              />
           );
         })}
       </StyledWrapper>
