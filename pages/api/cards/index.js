9b1dc701de74eb071c5da57eecdf3a1db30d8c1f
Daniel <cyubahirodaniel@gmail.com>
2025-05-09 15:00:37 +0200

    Include db connection inside try and catch in api/cards

--- pages/api/cards/index.js
+++ pages/api/cards/index.js
@@ -8,20 +8,15 @@ export default async function handler(request, response) {
       const cards = await Cards.find().populate('collectionId');
       return response.status(200).json(cards);
     }
-  } catch (error) {
-    console.error('Failed to GET cards \nAPI Error:', error);
-    return response.status(500).json({error: error.message});
-  }
 
-  try {
     if (request.method === 'POST') {
       const cardData = request.body;
       const newCard = await Cards.create(cardData);
       return response.status(201).json(newCard);
     }
+    return response.status(405).json({message: 'Method not allowed'});
   } catch (error) {
-    console.error('Failed to create card \nAPI Error:', error);
+    console.error('API Error:', error);
     return response.status(500).json({error: error.message});
   }
-  return response.status(405).json({ message: "Method not allowed" });
 }
