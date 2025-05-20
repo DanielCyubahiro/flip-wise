1a6430695ce4d7d1be68f11aa5e7ffa55123f206
Daniel <cyubahirodaniel@gmail.com>
2025-05-20 11:02:24 +0200

    Add back github provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,11 @@
 import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
-  providers: [CredentialsProvider({
+  providers: [
+    process.env.VERCEL_ENV === "preview"
+        ? CredentialsProvider({
           name: "credentials",
           credentials: {
             username: { label: "Username", type: "text", placeholder: "username" },
@@ -23,6 +26,10 @@ export const authOptions = {
             }
           },
         })
+        : GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
   ],
 };
 
