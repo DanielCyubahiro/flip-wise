0041f8866d518a41707cebe37407ba01173f343b
Daniel <cyubahirodaniel@gmail.com>
2025-05-21 11:20:40 +0200

    Add start game button

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,12 @@
 import styled from 'styled-components';
 import Login from '@/components/Login';
+import {StyledButton} from '@/components/StyledButton';
+import Link from 'next/link';
 
 const StyledContainer = styled.section`
     display: flex;
+    flex-direction: column;
+    gap: 2rem;
     justify-content: center;
     align-items: center;
     min-height: 90vh;
@@ -10,6 +14,9 @@ const StyledContainer = styled.section`
 const HomePage = () => {
   return (
       <StyledContainer>
+        <Link href={'/collections'}>
+          <StyledButton>Start Game</StyledButton>
+        </Link>
         <Login/>
       </StyledContainer>
   );
