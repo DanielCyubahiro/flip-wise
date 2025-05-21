3b61ab57b1281294da972af4d4b2ccb8c009bac6
Daniel <cyubahirodaniel@gmail.com>
2025-05-21 10:40:15 +0200

    Fix redirection

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,12 +1,16 @@
 import {signIn, useSession} from 'next-auth/react';
 import {StyledButton} from '@/components/StyledButton';
 import Logout from '@/components/Logout';
+import {useRouter} from 'next/router';
 
 export default function Login() {
   const { data: session } = useSession();
+  const router = useRouter();
 
   if (session) {
     return <Logout/>
   }
-  return <StyledButton onClick={() => signIn("github", { callbackUrl: "/cards" })}>Sign in</StyledButton>
+
+  const callbackUrl = router.query.callbackUrl || router.asPath || '/';
+  return <StyledButton onClick={() => signIn("github", { callbackUrl })}>Sign in</StyledButton>
 }
