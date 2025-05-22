2da5063c4a24b40bbb8c64f17c0e577c4524ba51
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-05-22 19:11:31 +0200

    footer designed

--- components/Logout.jsx
+++ components/Logout.jsx
@@ -3,21 +3,33 @@ import { StyledButton } from '@/components/StyledButton'
 import styled from 'styled-components'
 
 const StyledLogoutButton = styled(StyledButton)`
-  background: black;
-  color: white;
-  border-style: none;
-  padding: 0.8rem 1.5rem;
+  display: inline-block;
+  padding: 0.85rem 1.3rem;
   border-radius: 0.6rem;
+  text-decoration: none;
   font-weight: bold;
+  border: 1px solid black;
+  transition: background-color 0.2s, border-color 0.2s;
   font-size: 1rem;
+  margin: 0 6px;
+
+  color: white;
+  background-color: ${({ $active }) =>
+    $active ? 'var(--color-button-two)' : 'var(--color-button-one)'};
+
+  &:hover {
+    background-color: #4a4949;
+    border-color: #1a1a1a;
+  }
 `
+
 const Logout = () => {
   const { data: session } = useSession()
 
   if (session) {
     return (
       <StyledLogoutButton onClick={() => signOut({ callbackUrl: '/cards' })}>
-        Sign out
+        Log out
       </StyledLogoutButton>
     )
   }
