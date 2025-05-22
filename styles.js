c8b783d082aa562f05c1780d3c6a8f89a29458d3
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-05-22 11:14:55 +0200

    add variables

--- styles.js
+++ styles.js
@@ -7,6 +7,15 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
 
+:root {
+  --color-primary: #000000;           
+  --color-secondary: #ffff00;         
+  --color-tertiary: #65574c;          
+  --color-quaternary: #714E33;        
+  --color-quinary: #1C1B26;           
+  --color-senary: #414557;
+}
+
   body {
     margin: 0;
     font-family: system-ui;
