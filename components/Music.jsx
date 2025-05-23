d8d1ec43e4c425efad479ce6210f22b8d52ff05f
Adam Harris <adamtjharris@gmail.com>
2025-05-23 13:31:25 +0200

    readded to avoid conflict

---
+++ components/Music.jsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+import { useEffect, useRef } from 'react'
+
+const BackgroundMusic = styled.audio`
+  display: none;
+`
+
+export default function Music() {
+  const audioRef = useRef(null)
+  useEffect(() => {
+    const audio = audioRef.current
+    if (audio) {
+      audio.volume = 0.5
+      audio.loop = true
+      audio.autoplay = true
+      window.backgroundMusic = audio
+
+      audio.play().catch(() => {
+        console.log('Autoplay blocked')
+      })
+    }
+  }, [])
+
+  return (
+    <BackgroundMusic ref={audioRef} autoPlay loop style={{ display: 'none' }}>
+      <source src="/sound/HP-theme.mp3" type="audio/mpeg" />
+      Your browser does not support the audio element.
+    </BackgroundMusic>
+  )
+}
